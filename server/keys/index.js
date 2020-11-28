88c843012e5dae3d8c7a3c030b7528e516d5c33c
buriymishka <55351111+buriymishka@users.noreply.github.com>
2020-11-28 22:07:15 +0200

    make it better

--- server/keys/index.js
+++ server/keys/index.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./keys.prod')
+  module.exports = require('./keys.prod.js')
 } else {
   module.exports = require('./keys.dev')
 }
