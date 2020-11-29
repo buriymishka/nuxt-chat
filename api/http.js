9b21ce3f07080897c616f7da58d0f347e7d1915d
buriymishka <55351111+buriymishka@users.noreply.github.com>
2020-11-29 11:02:49 +0200

    make it better

--- api/http.js
+++ api/http.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 import * as tokensAPI from './tokens'
 
 const instance = axios.create({
-  baseURL: 'http://localhost:3000/api/',   //  https://mihail-nuxt-chat.herokuapp.com/api/
+  baseURL: 'https://mihail-nuxt-chat.herokuapp.com/api/',   // http://localhost:3000/api/
   withCredentials: true
 })
 
--- plugins/sockets.js
+++ plugins/sockets.js
@@ -3,7 +3,7 @@ import io from 'socket.io-client'
 import VueSocketIO from 'vue-socket.io-extended'
 
 export default ({ store }) => {
-  Vue.use(VueSocketIO, io('http://localhost:3000'),   //  https://mihail-nuxt-chat.herokuapp.com/
+  Vue.use(VueSocketIO, io('https://mihail-nuxt-chat.herokuapp.com/'),   // http://localhost:3000
     {
       store,
       debug: false,
